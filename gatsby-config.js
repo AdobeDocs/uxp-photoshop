062a9cc68ab6a6ef76f1f6d3b7b32bf4a581264e
Stephan Ringel <ringel@adobe.com>
2021-03-18 12:56:54 +0100

    Update to Gatsby v3

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,18 +10,8 @@
  * governing permissions and limitations under the License.
  */
 
-const fs = require("fs");
-
-const globalNavFilePath = require.resolve(
-  "@adobe/gatsby-theme-aio/globalNav.json"
-);
-const globalNav = JSON.parse(fs.readFileSync(globalNavFilePath, "utf8"));
-
-globalNav.menus = [globalNav.menus[2]];
-
 module.exports = {
   siteMetadata: {
-    globalNav,
     /*versions: [
       {
         title: 'v4.1'
