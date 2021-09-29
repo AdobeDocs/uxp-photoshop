9ad24692fdf397a4df7a6305c69db9a7d001291c
Barkin Aygun <baaygun@adobe.com>
2021-09-29 15:51:09 -0700

    Correct reference

--- reference-ps.js
+++ reference-ps.js
@@ -1,52 +1,219 @@
-module.exports = [
-  {
-    title: 'Modules',
-    path: '/ps_reference/classes/',
-    header: true,
-    pages: [
-      {
-        title: 'Photoshop',
-        path: '/ps_reference/classes/photoshop/'
-      },
-      {
-        title: 'Document',
-        path: '/ps_reference/classes/document/'
-      },
-      {
-        title: 'Layer',
-        path: '/ps_reference/classes/layer/'
-      },
-      {
-        title: 'Action',
-        path: '/ps_reference/classes/action/'
-      },
-      {
-        title: 'ActionSet',
-        path: '/ps_reference/classes/actionset/'
-      }
-    ]
-  },
-  {
-    title: 'Advanced',
-    header: true,
-    path: '/ps_reference/media/advanced/',
-    pages: [
-      {
-        title: 'BatchPlay',
-        path: '/ps_reference/media/advanced/batchplay/'
-      },
-      {
-        title: 'API Extensions',
-        path: '/ps_reference/media/advanced/prototype/'
-      },
-      {
-        title: 'CSDK <-> UXP Messaging',
-        path: '/ps_reference/media/advanced/cpp-pluginsdk/'
-      },
-      {
-        title: 'Event Listeners',
-        path: '/ps_reference/media/advanced/event-listener/'
-      }
-    ]
-  }
+module.exports = [{
+	"title": "Classes",
+	"path": "/ps_reference/classes/",
+	"header": true,
+	"pages": [
+		{
+			"title": "Action",
+			"path": "/ps_reference/classes/action/"
+		},
+		{
+			"title": "ActionSet",
+			"path": "/ps_reference/classes/actionset/"
+		},
+		{
+			"title": "Channel",
+			"path": "/ps_reference/classes/channel/"
+		},
+		{
+			"title": "Document",
+			"path": "/ps_reference/classes/document/"
+		},
+		{
+			"title": "Guide",
+			"path": "/ps_reference/classes/guide/"
+		},
+		{
+			"title": "HistoryState",
+			"path": "/ps_reference/classes/historystate/"
+		},
+		{
+			"title": "Layer",
+			"path": "/ps_reference/classes/layer/"
+		},
+		{
+			"title": "Photoshop",
+			"path": "/ps_reference/classes/photoshop/"
+		},
+		{
+			"title": "Channels",
+			"path": "/ps_reference/classes/channels/"
+		},
+		{
+			"title": "Documents",
+			"path": "/ps_reference/classes/documents/"
+		},
+		{
+			"title": "Guides",
+			"path": "/ps_reference/classes/guides/"
+		},
+		{
+			"title": "HistoryStates",
+			"path": "/ps_reference/classes/historystates/"
+		},
+		{
+			"title": "Layers",
+			"path": "/ps_reference/classes/layers/"
+		},
+		{
+			"title": "TextFonts",
+			"path": "/ps_reference/classes/textfonts/"
+		},
+		{
+			"title": "SolidColor",
+			"path": "/ps_reference/classes/solidcolor/"
+		},
+		{
+			"title": "TextFont",
+			"path": "/ps_reference/classes/textfont/"
+		}
+	]
+},
+    {
+	"title": "Objects",
+	"path": "/ps_reference/objects/",
+	"header": true,
+	"pages": [
+		{
+			"title": "Bounds",
+			"path": "/ps_reference/objects/bounds/"
+		},
+		{
+			"title": "BMPSaveOptions",
+			"path": "/ps_reference/objects/bmpsaveoptions/"
+		},
+		{
+			"title": "BitmapConversionOptions",
+			"path": "/ps_reference/objects/bitmapconversionoptions/"
+		},
+		{
+			"title": "GIFSaveOptions",
+			"path": "/ps_reference/objects/gifsaveoptions/"
+		},
+		{
+			"title": "IndexedConversionOptions",
+			"path": "/ps_reference/objects/indexedconversionoptions/"
+		},
+		{
+			"title": "JPEGSaveOptions",
+			"path": "/ps_reference/objects/jpegsaveoptions/"
+		},
+		{
+			"title": "PNGSaveOptions",
+			"path": "/ps_reference/objects/pngsaveoptions/"
+		},
+		{
+			"title": "PhotoshopSaveOptions",
+			"path": "/ps_reference/objects/photoshopsaveoptions/"
+		},
+		{
+			"title": "DocumentCreateOptions",
+			"path": "/ps_reference/objects/documentcreateoptions/"
+		},
+		{
+			"title": "GroupLayerCreateOptions",
+			"path": "/ps_reference/objects/grouplayercreateoptions/"
+		},
+		{
+			"title": "LayerCreateOptions",
+			"path": "/ps_reference/objects/layercreateoptions/"
+		},
+		{
+			"title": "BMPSaveOptions",
+			"path": "/ps_reference/objects/bmpsaveoptions/"
+		},
+		{
+			"title": "GIFSaveOptions",
+			"path": "/ps_reference/objects/gifsaveoptions/"
+		},
+		{
+			"title": "JPEGSaveOptions",
+			"path": "/ps_reference/objects/jpegsaveoptions/"
+		},
+		{
+			"title": "PNGSaveOptions",
+			"path": "/ps_reference/objects/pngsaveoptions/"
+		},
+		{
+			"title": "PhotoshopSaveOptions",
+			"path": "/ps_reference/objects/photoshopsaveoptions/"
+		},
+		{
+			"title": "Tool",
+			"path": "/ps_reference/objects/tool/"
+		}
+	]
+},
+    {
+	"title": "Colors",
+	"path": "/ps_reference/colors/",
+	"header": true,
+	"pages": [
+		{
+			"title": "CMYKColor",
+			"path": "/ps_reference/colors/cmykcolor/"
+		},
+		{
+			"title": "GrayColor",
+			"path": "/ps_reference/colors/graycolor/"
+		},
+		{
+			"title": "HSBColor",
+			"path": "/ps_reference/colors/hsbcolor/"
+		},
+		{
+			"title": "LabColor",
+			"path": "/ps_reference/colors/labcolor/"
+		},
+		{
+			"title": "NoColor",
+			"path": "/ps_reference/colors/nocolor/"
+		},
+		{
+			"title": "RGBColor",
+			"path": "/ps_reference/colors/rgbcolor/"
+		}
+	]
+},
+    {
+	"title": "Advanced",
+	"header": true,
+	"path": "/ps_reference/media/",
+	"pages": [
+		{
+			"title": "BatchPlay",
+			"path": "/ps_reference/media/batchplay/"
+		},
+		{
+			"title": "Modal Execution",
+			"path": "/ps_reference/media/executeAsModal/"
+		},
+		{
+			"title": "API Extensions",
+			"path": "/ps_reference/media/prototype/"
+		},
+		{
+			"title": "CSDK <-> UXP Messaging",
+			"path": "/ps_reference/media/cpp-pluginsdk/"
+		},
+		{
+			"title": "Core Module",
+			"path": "/ps_reference/media/photoshopCore/"
+		},
+		{
+			"title": "Action Module",
+			"path": "/ps_reference/media/photoshopAction/"
+		}
+	]
+},
+    {
+	"title": "Constants",
+	"header": true,
+	"path": "/ps_reference/modules/constants/"
+},
+    {
+	"title": "API Changelog",
+	"header": true,
+	"path": "/ps_reference/changelog/"
+}
 ]
